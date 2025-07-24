95ba951d078a49e814287dd7fdffb906a533ef48
gaurav sharma <gauravsharma111199@gmail.com>
2025-07-24 01:07:29 -0400

    deploy portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true }, // Optional: disables Image Optimization
+  basePath: '/portfolio-gaurav', // Replace with your actual repo name
+  trailingSlash: true,
+};
 
 export default nextConfig;
